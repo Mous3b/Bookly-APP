d712dd2359426fadc5419446d8424fff91e0cdfe
Mous3b <mousabsamir330@gmail.com>
2024-01-03 22:22:27 +0200

    splashViewBody

--- lib/Features/Splash/presentation/views/widgets/splash_view_body.dart
+++ lib/Features/Splash/presentation/views/widgets/splash_view_body.dart
@@ -5,8 +5,6 @@ class SplashViewbody extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Container(
-      child: Text('hi'),
-    );
+    return Container();
   }
 }
