ea374c9e4b344da0a007651cbe6a04d6998615c5
Mous3b <mousabsamir330@gmail.com>
2024-01-07 22:08:41 +0200

    book entity

---
+++ lib/Features/home/domain/entities/bookEntity.dart
@@ -0,0 +1,14 @@
+class BookEntity {
+  final String img;
+  final String title;
+  final String authorName;
+  final num price;
+  final num rating;
+
+  BookEntity(
+      {required this.img,
+      required this.title,
+      required this.authorName,
+      required this.price,
+      required this.rating});
+}
